b7e531b011fc5329d6b855620dd507b475272311
Milan <milanvpage@gmail.com>
2021-05-19 00:54:13 -0700

    commit tree ebf83faaddcde4824abe46c31c276ba686797329

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Milan"
+    end 
+
+    get '/hometown' do
+        "My hometown is Portland"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Bubble Toes"
+    end 
+      
+      
+      
 end
